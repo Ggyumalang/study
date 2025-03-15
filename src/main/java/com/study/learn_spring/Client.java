206e953990c25b2040d84a4efba640eea1986bf0
Ggyumalang <khg2154@naver.com>
2025-03-15 10:29:05 +0900

    11. 스프링 컨테이너와 의존 관계 주입

--- src/main/java/com/study/learn_spring/Client.java
+++ src/main/java/com/study/learn_spring/Client.java
@@ -1,13 +1,19 @@
 package com.study.learn_spring;
 
+import org.springframework.beans.factory.BeanFactory;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
 import java.math.BigDecimal;
 
 //관계 설정 책임의 분리
 public class Client {
     public static void main(String[] args) {
         // 객체 설정 및 관계 설정
-        ObjectFactory objectFactory = new ObjectFactory();
-        PaymentService paymentService = objectFactory.paymentService();
+        BeanFactory beanFactory = new AnnotationConfigApplicationContext(PaymentConfig.class);
+        PaymentService paymentService = beanFactory.getBean(PaymentService.class);
+//
+//        ObjectFactory objectFactory = new ObjectFactory();
+//        PaymentService paymentService = objectFactory.paymentService();
 
         //객체 사용
         Payment payment = paymentService.prepare(100L, "USD", BigDecimal.valueOf(50.7));
