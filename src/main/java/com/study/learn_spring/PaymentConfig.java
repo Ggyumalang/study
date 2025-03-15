0a22ba07b988b3c507df93717a7f27730d09c471
Ggyumalang <khg2154@naver.com>
2025-03-15 10:29:12 +0900

    11. 스프링 컨테이너와 의존 관계 주입

--- src/main/java/com/study/learn_spring/PaymentConfig.java
+++ src/main/java/com/study/learn_spring/PaymentConfig.java
@@ -1,12 +1,16 @@
 package com.study.learn_spring;
 
-// 객체 생성 기능 클래스 = 객체 생성 + 관계 설정
-public class ObjectFactory {
+import org.springframework.context.annotation.Bean;
 
+// 객체 생성 기능 클래스 = 객체 생성 + 관계 설정
+public class PaymentConfig {
+
+    @Bean
     public PaymentService paymentService() {
         return new PaymentService(exchangeRateProvider());
     }
 
+    @Bean
     public ExchangeRateProvider exchangeRateProvider() {
         return new HttpApiExchangeRateProvider();
     }
